d7184116d6f137f39033131f2ba817eef2900ab5
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-08-23 13:19:05 +0530

    forms added

--- src/App.js
+++ src/App.js
@@ -23,7 +23,10 @@ export default function App() {
   return <div className="app">
     <div className="sidebar">
       <FriendsList />
+      <FormAddFriend />
+      <Button>Add Friend</Button>
     </div>
+    <FormSplitBill />
   </div>
 }
 
@@ -48,6 +51,44 @@ function Friend({ friend }) {
 
     {friend.balance > 0 && <p className="green">{`${friend.name} owes you $${Math.abs(friend.balance)}`}</p>}
 
-    <button className="button">Select</button>
+    <Button>Select</Button>
   </li>
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>
+}
+
+function FormAddFriend() {
+  return <form className="form-add-friend">
+    <label>👯Friend name</label>
+    <input type="text" />
+
+    <label>📷 Image URL</label>
+    <input type="text" />
+
+    <Button>Add</Button>
+  </form>
+}
+
+function FormSplitBill() {
+  return <form className="form-split-bill">
+    <h2>Split a bill with X</h2>
+
+    <label>💰 Bill Value</label>
+    <input type="text"></input>
+
+    <label>👨 Your expense</label>
+    <input type="text"></input>
+
+    <label>👯 X's expense</label>
+    <input type="text"></input>
+
+    <label>🤑 Who is paying the bill ?</label>
+    <select>
+      <option value="user">You</option>
+      <option value="friend">X</option>
+    </select>
+
+  </form>
 }
